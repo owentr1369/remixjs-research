78ff2b67981ca43cedf9ac3695bade7fcf585fa2
TranTam <tamvaa1306@gmail.com>
2025-01-15 16:36:45 +0700

    refactor: update index layout and styles, remove demo page, and add notes page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "@remix-run/react";
 
+import homeStyles from "~/styles/home.css";
+
 const Index = () => {
   return (
-    <div>
-      <div>Hello world!</div>
-      <Link to="/demo">Go to demo page</Link>
-    </div>
+    <main id="content">
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try out early beta and never loose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p>
+    </main>
   );
 };
+
+export function links() {
+  return [{ rel: "stylesheet", href: homeStyles }];
+}
 
 export default Index;
--- app/routes/demo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Demo = () => {
-  return <div>Demo Page</div>;
-};
-
-export default Demo;
